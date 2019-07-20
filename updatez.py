ea1a74d389ae085a666b8c36fd1ac5a5bd92b316
n8programs <43304488+N8python@users.noreply.github.com>
2019-07-20 08:49:32 -0400

    Create updatez.py

---
+++ updatez.py
@@ -0,0 +1,6 @@
+import os
+os.system("cd ~")
+os.system("sudo npm uninstall -g @zlanguage/zcomp")
+os.system("sudo npm install -g @zlanguage/zcomp")
+os.system("sudo npm uninstall -g @zlanguage/zstdlib")
+os.system("sudo npm install -g @zlanguage/zstdlib")
